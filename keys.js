7d131947775c7c2605f60027e454dbc6be2381df
Frank Valdez <fvaldez421@gmail.com>
2018-02-14 20:29:26 -0800

    added and formatted all keys

--- keys.js
+++ keys.js
@@ -1,12 +1,23 @@
 
-console.log("this is loaded");
+console.log("keys are loaded")
 
 var twitterKeys = {
-	consumer_key: "",
-	consumer_secret: "",
-	access_token_key: "",
-	access_token_secret: "",
+	consumer_key: "OryXTyIEdOIIIK3J2EMdPiVNL",
+	consumer_secret: "koTrY4uLFhS1XSIte463PhJQxNtBihDElvbnKUFERfAgJNQL72",
+	access_token_key: "963250119557529600-KeOLFLy7v6t35azYP7VIyKlEC3clkVR",
+	access_token_secret: "yQhRIvHuggjJhf0eoPc2wOQB5Dn4a5P2YOL5JL9r1gsyf"
 }
 
-module.exports = twitterKeys;
+var spotifyKeys = {
+	id: "f3ba3cf29329417185923a9750b23055",
+	secret: "3e8a9d6d57704b7ab945a4450985565d"
+}
+
+var omdbKey = "a75562ad";
+
+module.exports = {
+	twitterKeys: twitterKeys,
+	spotifyKeys: spotifyKeys,
+	omdbKey: omdbKey
+}
 
